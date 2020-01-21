141e308d1f0dce710b76b35c672a6e844686d774
Yahya  Gok <ygok17@hotmail.com>
2020-01-21 16:30:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  Array = ["Yahya", "Betul","anne","samet"]
+  ["Yahya", "Betul","anne","samet"]
 end
 
 def add_element_to_end_of_array(array, element)
